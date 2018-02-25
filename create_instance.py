8131d0fc01d5065f0cef117393eea5b067c3efab
David Hart <dhart001@gmail.com>
2018-02-24 16:41:56 -0800

    added security group

--- create_instance.py
+++ create_instance.py
@@ -3,8 +3,9 @@ import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
     ImageId='ami-f2d3638a',
-    MinCount=3,
-    MaxCount=3,
+    MinCount=1,
+    MaxCount=1,
+    KeyName='davetest-key',
     InstanceType='t2.micro')
 print instance[0].id
 
