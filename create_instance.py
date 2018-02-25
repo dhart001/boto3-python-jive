39b5dadfbf01397df2f74e1ec66f4f484f2a6259
David Hart <dhart001@gmail.com>
2018-02-24 17:22:28 -0800

    corrected ami for ecs optimized

--- create_instance.py
+++ create_instance.py
@@ -2,7 +2,7 @@
 import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
-    ImageId='ami-10ed6968',
+    ImageId='ami-92e06fea',
     MinCount=1,
     MaxCount=1,
     KeyName='davetest-key',
